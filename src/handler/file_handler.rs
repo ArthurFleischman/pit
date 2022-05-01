166f8127a44dc02de6714489012fb48ec2d4a9b0
Arthur Fleischman <arthur@pop-os.localdomain>
2022-05-01 01:34:38 -0300

    creating constructor for FileData and parsing functions str->json->FileData

--- src/handler/file_handler.rs
+++ src/handler/file_handler.rs
@@ -1,8 +1,11 @@
 use serde::{Serialize,Deserialize};
 
 #[derive(Serialize, Deserialize)]
-pub struct FileData {
+struct FileData {
     pub initial_commands: Vec<String>,
     pub default_structure: Vec<String>,
     pub default_files: Vec<String>
+}
+pub fn new(initial_commands: Vec<String>,default_structure: Vec<String>,default_files: Vec<String>)-> FileData{
+    FileData { initial_commands: initial_commands, default_structure: default_structure, default_files: default_files }
 }
--- src/handler/project_handler.rs
+++ src/handler/project_handler.rs
@@ -1,11 +1,15 @@
 use std::{fs, io::Read};
-use serde_json;
+use serde_json::Value;
 
 use crate::args::project_args::ProjectArgs;
 
+use super::file_handler;
+
 pub fn handle_project(args: ProjectArgs){
     create_root(args.name, args.path);
-    get_lang_structure(args.lang);
+    let raw_json_data = get_lang_structure(args.lang);
+    let parsed_data = parse_by_app_type(args.typ, raw_json_data);
+
 }
 
 fn create_root(name: String, path:String){
@@ -15,11 +19,15 @@ fn create_root(name: String, path:String){
         Ok(_)=>println!("projects root dir created successfully at:\n{}",path_name)
     };
 }
-fn get_lang_structure(lang: String) -> serde_json::Value{
+fn get_lang_structure(lang: String) -> Value{
     let config_path =format!("./config/{}.json",lang);
     let mut file = fs::File::open(config_path).expect("[FS] could not open file");
     let mut string_data = String::new();
     file.read_to_string(&mut string_data).expect("[FILE READER] could not read data");
     serde_json::from_str(string_data.as_str()).expect("[SERDE] could not parse file to json")
+}
+fn parse_by_app_type(app_type: String, raw_json_data: Value) -> FileData{
+    let app_data = raw_json_data[app_type].as_object().expect("[SERDE} could not parse json app to map");
+    // file_handler::new(app_data["initial_commands"].as_array_mut(), default_structure, default_files)
 }
 
